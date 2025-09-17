8ad80a50073eee576887aaf8121ced83fb05b793
Rewriter <zonestore.vip@gmail.com>
2025-09-17 14:06:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 const CONFIG = {
   maintenance: true,  // change to false to disable
-  message: "Project Original is currently under maintenance. Please check back later!"
+  message: "🚧 Project Original is Currently Under Maintenance 🚧
+    The Battle Bus is grounded and our servers are taking a quick pit stop We’re patching up bugs, upgrading loot drops, and making sure your next drop-in is smoother than ever.
+✨ Don’t worry — we’ll be back online soon.
+In the meantime, grab some snacks, stretch those build fingers, and get ready for the next victory royale.
+Thank you for your patience, legend. 💜
+— Project Original Team"
 };
